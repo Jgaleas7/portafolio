9e58743a7b4251fd48cd480776a20ddc414cba83
Juan Galeas <juangaleassantos@gmail.com>
2025-08-19 19:04:50 -0700

    style: show article cards in grid

---
+++ pages/articles/_slug.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <nuxt-content :document="article" />
+  </v-container>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
+    return { article }
+  }
+}
+</script>
---
+++ pages/articles/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="article in articles"
+        :key="article.slug"
+        cols="12"
+        sm="6"
+        md="4"
+      >
+        <v-card
+          nuxt
+          :to="`/articles/${article.slug}`"
+          class="ma-2"
+          outlined
+        >
+          <v-card-title>{{ article.title }}</v-card-title>
+          <v-card-text>{{ article.description }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const articles = await $content('articles').sortBy('createdAt', 'desc').fetch()
+    return { articles }
+  }
+}
+</script>
