9a0a5b62aabd353e6be6b565f0627ca5ef9b1ac8
Juan Galeas <juangaleassantos@gmail.com>
2025-08-19 18:52:22 -0700

    feat: add markdown articles and responsive hero image

---
+++ content/articles/hello-world.md
@@ -0,0 +1,8 @@
+---
+title: Hello World
+description: First article
+---
+
+# Hello World
+
+This is the first article written in **markdown**.
---
+++ pages/articles/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="article in articles"
+        :key="article.slug"
+        cols="12"
+      >
+        <nuxt-link :to="`/articles/${article.slug}`">
+          <h2>{{ article.title }}</h2>
+        </nuxt-link>
+        <p>{{ article.description }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const articles = await $content('articles').sortBy('createdAt', 'desc').fetch()
+    return { articles }
+  }
+}
+</script>
