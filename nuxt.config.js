5dd9a7de5d82fb4c2331928881894f1b3c1e7a84
Juan Galeas <juangaleassantos@gmail.com>
2025-10-24 18:50:38 -0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -95,7 +95,7 @@ dir: {
       transformMixedEsModules: true, 
       // If a specific package is causing the issue, we can list it here to force inclusion/processing
       include: /node_modules/ 
-    }
+    },
   /*
   ** vuetify module configuration for Nuxt 3
   ** This is completely different from Nuxt 2
